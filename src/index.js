826c721c207d2bae0e24c89957fe26a446182ca5
Richard Douglas <r.douglas.42092@gmail.com>
2023-07-01 12:11:14 -0700

    initial commit. same codebase as resumegenerator (at the moment). separating front and back.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+const API_URL = process.env.API_URL || 'http://localhost'
+const SOCK_URL = process.env.SOCK_URL || 'WSS://localhost'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
