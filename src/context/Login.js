020a0b7b5289f1652e4ede55194a021991ce5d28
Richard Douglas <r.douglas.42092@gmail.com>
2023-07-01 12:48:17 -0700

    initial commit. same codebase as resumegenerator (at the moment). separating front and back.

--- src/context/Login.js
+++ src/context/Login.js
@@ -3,11 +3,11 @@ import React, { useState, useContext } from 'react'
 import { UserContext } from './UserContext'
 import { useNavigate } from "react-router-dom"
 import io from 'socket.io-client'
-const aws = require('aws-sdk');
-let s3 = new aws.S3({
-  accessKeyId: process.env.S3_KEY,
-  secretAccessKey: process.env.S3_SECRET
-});
+// const aws = require('aws-sdk');
+// let s3 = new aws.S3({
+//   accessKeyId: process.env.S3_KEY,
+//   secretAccessKey: process.env.S3_SECRET
+// });
 const API_URL = process.env.API_URL 
 const SOCK_URL = process.env.SOCK_URL || 'WSS://localhost'
 
