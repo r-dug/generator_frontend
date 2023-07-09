4281d53958e7b30a83df6695f4b3df5c347536ac
Richard Douglas <r.douglas.42092@gmail.com>
2023-07-09 11:16:39 -0700

    persistent login. hope it works on heroku.

--- src/components/CookieRemoval.js
+++ src/components/CookieRemoval.js
@@ -7,10 +7,17 @@ const deleteCookies = () => {
     Object.keys(Cookies.get()).forEach(function(cookieName) {
     const now = Date.now()
     var neededAttributes = {
+        proxy: true,
+        sameSite: 'none', // cross-site
+        secure: true, // Set to true if using HTTPS
+        httpOnly: false, // Prevent client-side JavaScript from accessing cookies
         expires: 'Thu, 01 Jan 1970 00:00:00 UTC', // Session expiration time (in milliseconds)
+        domain: process.env.COOKIE_ALLOW,
+        path: "/"
     }
     console.log(cookieName, neededAttributes)
     Cookies.set(cookieName, neededAttributes)
+    Cookies.remove(cookieName)
     })}
 
 export default deleteCookies 
