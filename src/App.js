3623f7c9638288e8eb19cf8cb1cb334ad7d4df17
Richard Douglas <r.douglas.42092@gmail.com>
2023-07-08 18:04:03 -0700

    persistent login. hope it works on heroku.

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ const App = () => {
         let gettingStores = browser.cookies.getAllCookieStores()
         console.log(gettingStores)
   const sessionCookie = Cookies.get('session')
+  console.log(sessionCookie)
   const check = cookieCheck(sessionCookie)
   const Navigation = () => {
     return (
