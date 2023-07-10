38ad0f2ed7e56a7dedc33b6798a952562e6590a0
Richard Douglas <r.douglas.42092@gmail.com>
2023-07-10 11:38:08 -0700

    persistent login. I think it will work on heroku.

--- src/components/CookieRemoval.js
+++ src/components/CookieRemoval.js
@@ -9,23 +9,25 @@ function del(cookieList) {
         let split = cookies[0].split("=")
         console.log("cookie type: ",typeof split)
         document.cookie = `${split}; expires= ${new Date(0).toUTCString()};domain=resume-generator.info;path='/';secure=true;httpOnly=false;`
-        console.log(document.cookie)
+        console.log(1, document.cookie)
         document.cookie = `${split}; expires= ${new Date(0).toUTCString()};path='/';secure=true;httpOnly=false;`
-        console.log(document.cookie)
+        console.log(2, document.cookie)
+        document.cookie = `${split}; expires= ${new Date(0).toUTCString()};path='/'`
+        console.log(3, document.cookie)
         document.cookie = `${split}; expires= ${new Date(0).toUTCString()}`
-        console.log(document.cookie)
+        console.log(4, document.cookie)
         let name = split[0]
         console.log("name to delete",name)
         Cookies.remove(name)
         console.log(new Date(0).toUTCString())
-        let newCookie = `${name}=; expires= ${new Date(0).toUTCString()};path='/';secure=true;httpOnly=false;`
-        console.log(newCookie)
+        let newCookie = `${name}=; expires= ${new Date(0).toUTCString()}`
+        console.log(5, newCookie)
         document.cookie = `${name}=; expires= ${new Date(0).toUTCString()};domain=resume-generator.info;path='/';secure=true;httpOnly=false;`
-        console.log(document.cookie)
+        console.log(6, document.cookie)
         document.cookie = `${name}=; expires= ${new Date(0).toUTCString()};path='/';secure=true;httpOnly=false;`
-        console.log(document.cookie)
+        console.log(7, document.cookie)
         document.cookie = `${name}=; expires= ${new Date(0).toUTCString()};path='/'`
-        console.log(document.cookie)
+        console.log(8, document.cookie)
         cookies.shift()
     }
     // document.cookie = name + `=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;domain=${process.env.COOKIE_ALLOW};samesite=none;secure=true;`;
