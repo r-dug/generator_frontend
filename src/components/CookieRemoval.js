625ad30e4621e32f1e079a798b30aa58420879ac
Richard Douglas <r.douglas.42092@gmail.com>
2023-07-10 11:23:59 -0700

    persistent login. I think it will work on heroku.

--- src/components/CookieRemoval.js
+++ src/components/CookieRemoval.js
@@ -8,14 +8,15 @@ function del(cookieList) {
         console.log("cookie type: ",typeof cookies)
         let split = cookies[0].split("=")
         console.log("cookie type: ",typeof split)
-        document.cookie = `${split}; expires= ${new Date(0).toUTCString()};path='/';secure=true;httponly=false;`
+        document.cookie = `${split}; expires= ${new Date(0).toUTCString()};domain=resume-generator.info;path='/';secure=true;httponly=false;`
+        console.log(document.cookie)
         let name = split[0]
         console.log("name to delete",name)
         Cookies.remove(name)
         console.log(new Date(0).toUTCString())
         let newCookie = `${name}=; expires= ${new Date(0).toUTCString()};path='/';secure=true;httponly=false;`
         console.log(newCookie)
-        document.cookie = `${name}=; expires= ${new Date(0).toUTCString()};path='/';secure=true;httponly=false;`
+        document.cookie = `${name}=; expires= ${new Date(0).toUTCString()};domain=resume-generator.info;path='/';secure=true;httponly=false;`
         cookies.shift()
     }
     // document.cookie = name + `=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;domain=${process.env.COOKIE_ALLOW};samesite=none;secure=true;`;
